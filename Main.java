c35a6f590c215c7fc94b99df6aee9719f1ebccfd
justaspaz <55506752+justaspaz@users.noreply.github.com>
2024-12-16 04:17:29 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -23,7 +23,7 @@ public class Main {
 
         @Override
         protected Void compute() {
-            if (right - left < 1000) {
+            if (right - left < 1000000) {
                 Arrays.sort(array, left, right + 1); // Fallback to sequential sort
             } else {
                 int mid = (left + right) / 2;
